0455c38488e9c5ea3c89bd217d100d5dd4927346
graphitexhd <103639405+graphitexhd@users.noreply.github.com>
2022-04-20 00:04:04 +1000

    Updated Colour Picker Files

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 var colour1 = document.querySelector(".colour1");
 var colour2 = document.querySelector(".colour2");
 var css = document.querySelector('h3');
-var body= document.getElementById("gradient")
+var body= document.getElementById("gradient");
+
 
 colour1.addEventListener("input" , changeColorGradient);
 colour2.addEventListener("input" , changeColorGradient);
+
 
 function changeColorGradient(){ 
     body.style.background = "linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
     css.textContent = body.style.background;
 }
+
+window.onload = function(){
+    randomColourGradient();
+}
+
+var randomBtn = document.querySelector("#Randomizer");
+
+
+function random() {
+    return Math.floor(Math.random() * 13371337).toString(16);
+  }
+
+function randomColourGradient() {
+    colour1.value =  "#" + random();
+    colour2.value =  "#" + random();
+    changeColorGradient();
+}
+
+randomBtn.addEventListener("click", randomColourGradient);
+
+
+function copyText() {
+    var copyText = document.querySelector(".selectedGradient")
+    console.log(copyText.style.background);
+    navigator.clipboard.writeText(copyText.textContent);
+    alert("Copied the text: " + copyText.textContent);
+}
+
+
+
+
 
 
 
