7338153632cc29f4b1301f990ceda7ad6c0bb6d6
graphitexhd <103639405+graphitexhd@users.noreply.github.com>
2022-04-21 23:07:50 +1000

    Updates

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 var colour1 = document.querySelector(".colour1");
 var colour2 = document.querySelector(".colour2");
-var css = document.querySelector('h3');
+var colour3 = document.querySelector(".colour3");
+var cssBackground = document.querySelector('.cssBackground');
+var cssTextColour = document.querySelector('.cssTextColour');
 var body= document.getElementById("gradient");
 
 
 colour1.addEventListener("input" , changeColorGradient);
 colour2.addEventListener("input" , changeColorGradient);
+colour3.addEventListener("input" , changeTextColor);
 
 
 function changeColorGradient(){ 
     body.style.background = "linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
     body.style.background = "-webkit-linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
-    css.textContent = body.style.background;
-    
+    cssBackground.textContent = body.style.background;
+}
+function changeTextColor () {
+    body.style.color = colour3.value;
+    cssTextColour.textContent = body.style.color;
 }
 
 window.onload = function(){
     randomColourGradient();
+    changeTextColor();
 }
 
 var randomBtn = document.querySelector("#Randomizer");
 
 
 function random() {
-    return Math.floor(Math.random() * 13371337).toString(16);
+    return Math.floor(Math.random() * 16777215).toString(16);
   }
 
 function randomColourGradient() {
@@ -35,11 +42,15 @@ function randomColourGradient() {
 randomBtn.addEventListener("click", randomColourGradient);
 
 
-function copyText() {
-    navigator.clipboard.writeText(css.textContent);
-    alert("Background Colour Copied: " + css.textContent);
+function copyBackground() {
+    navigator.clipboard.writeText(cssBackground.textContent);
+    alert("Background Colour Copied: " + cssBackground.textContent);
 }
 
+function copyFontColour() {
+    navigator.clipboard.writeText(cssTextColour.textContent);
+    alert("Background Colour Copied: " + cssTextColour.textContent);
+}
 
 
 
