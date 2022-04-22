3b032163515ea596f76671bf4b81ca78bd94fd08
graphitexhd <103639405+graphitexhd@users.noreply.github.com>
2022-04-22 16:45:21 +1000

    Radiant Gradent Added
    Need to add radio buttons to switch between radial and linear

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var colour3 = document.querySelector(".colour3");
 var colour4 = document.querySelector(".colour4");
 var colour5 = document.querySelector(".colour5");
 var colour6 = document.querySelector(".colour6");
+var colour7 = document.querySelector(".colour7");
 var cssBackground = document.querySelector('.cssBackground');
 var cssTextColour = document.querySelector('.cssTextColour');
 var cssTextShadow = document.querySelector('.cssTextShadow');
@@ -13,7 +14,7 @@ var headingColour = document.querySelector(".headingColour");
 var headingColour2 = document.querySelector(".heading2");
 
 
-
+var rgb;
 
 colour1.addEventListener("input" , changeColorGradient);
 colour2.addEventListener("input" , changeColorGradient);
@@ -21,6 +22,7 @@ colour3.addEventListener("input" , changeTextColor);
 colour4.addEventListener("input" , changeHeadingTexts);
 colour5.addEventListener("input" , changeHeadingTexts);
 colour6.addEventListener("input" , changeHeadingTexts);
+colour7.addEventListener("input" , changeColorGradient);
 textHeadingSize.addEventListener("input" , changeHeadingTextSize);
 
 function changeHeadingTextSize(){
@@ -30,9 +32,24 @@ function changeHeadingTextSize(){
 }
 
 function changeColorGradient(){ 
-    body.style.background = "linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
-    body.style.background = "-webkit-linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
+    //linear-gradient
+    //body.style.background = "linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
+    //body.style.background = "-webkit-linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
+    //Radial Gradient
     cssBackground.textContent = body.style.background;
+    
+    var hex_code = colour1.value.split("");
+    var red = parseInt(hex_code[1]+hex_code[2],16);
+    var green = parseInt(hex_code[3]+hex_code[4],16);
+    var blue = parseInt(hex_code[5]+hex_code[6],16);
+    rgb = red+","+green+","+blue;
+    console.log(rgb)
+
+    body.style.background = "radial-gradient(rgba("+ rgb +", 0.5) 0%, transparent 70%), linear-gradient("+ colour2.value + " 40%, "+ colour7.value + " 70%)";
+    body.style.background = "-webkit-radial-gradient(rgba("+ rgb + ", 0.5) 0%, transparent 70%), linear-gradient("+ colour2.value + " 40%, "+ colour7.value + " 70%)";
+    
+    cssBackground.textContent = body.style.background;
+    
 }
 function changeTextColor () {
     body.style.color = colour3.value;
@@ -65,6 +82,7 @@ function random() {
 function randomColourGradient() {
     colour1.value =  "#" + random();
     colour2.value =  "#" + random();
+    colour7.value =  "#" + random();
     changeColorGradient();
 }
 
